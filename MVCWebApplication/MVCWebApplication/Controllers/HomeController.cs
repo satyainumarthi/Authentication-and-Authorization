69ade83c3d07c5fecb584c874e1429d4207afcc1
satyainumarthi <34768128+satyainumarthi@users.noreply.github.com>
2018-01-09 11:18:50 +0530

    MVC Web Application

---
+++ MVCWebApplication/MVCWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MVCWebApplication.Models;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MVCWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+    
+        [Authorize(Policy = "All")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "Admin")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
+    }
+}
---
+++ MVCWebApplication/MVCWebApplication/Startup.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MVCWebApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //Admin role can access both admin as well as editor resources
+            //Editor role can access only ediotr resources, admin resources are not available for editor
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("All", policy => policy.RequireRole("admin", "editor"));
+                options.AddPolicy("Admin", policy => policy.RequireRole("admin"));
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+            .AddCookie("Cookies")
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.SignInScheme = "Cookies";
+
+                options.Authority = "http://localhost:49381/";
+                options.RequireHttpsMetadata = false;
+
+                options.ClientId = "mvc";
+                options.Scope.Add("roles");
+                options.SaveTokens = true;
+            });
+     
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
